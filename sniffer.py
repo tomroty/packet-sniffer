373ccd9657837c0fc5f9055744e19cdc3336dc8e
tomroty <tom.roty@proton.me>
2025-03-18 21:12:05 +0100

    enhance unpack_ethernet_frame with detailed docstring and add get_mac_addr function for MAC address formatting

--- sniffer.py
+++ sniffer.py
@@ -2,8 +2,39 @@ import socket
 import struct
 import textwrap
 
-
 def unpack_ethernet_frame(data):
-    dest_mac, src_mac, protocol_type = struct.unpack("! 6s 6s H", data[:14]) # 6 bits + 6 bits + 2 bits
-    return dest_mac, src_mac, protocol_type, data[14:]
+    """
+    Unpacks the Ethernet frame
+    
+    Args: 
+        data: bytes
+            The raw data
+            
+    Returns:
+        dest: str
+            The destination MAC address
+        src: str
+            The source MAC address
+        protocol_type: int
+            The protocol type
+        data: bytes
+            The data
+    """
+    dest, src, protocol_type = struct.unpack("! 6s 6s H", data[:14]) # 6 bits + 6 bits + 2 bits
+    return get_mac_addr(dest), get_mac_addr(src), socket.htons(protocol_type), data[14:]
 
+def get_mac_addr(bytes_addr):
+    """
+    Converts the MAC address to a readable format
+    
+    Args:
+        bytes_addr: bytes
+            The MAC address
+            
+    Returns:
+        str
+            The MAC address in format AA:BB:CC:DD:EE:FF
+    """
+    bytes_str = map('{:02x}'.format, bin_str)
+    return ':'.join(bytes_str).upper
+    
