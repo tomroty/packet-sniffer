d920f98e603ad37190a6167befed4667e9f24963
tomroty <tom.roty@proton.me>
2025-03-18 23:50:06 +0100

    add IPv4 packet sniffing

--- sniffer.py
+++ sniffer.py
@@ -37,6 +37,51 @@ def get_mac_addr(bytes_addr):
     """
     return ":".join(map("{:02x}".format, bytes_addr))
 
+
+def unpack_ipv4_packet(data):
+    """
+    Unpacks the IPv4 packet
+    
+    Args:
+        data: bytes
+            The raw data
+            
+    Returns:
+        version: int
+            The version of the IP
+        header_length: int
+            The header length
+        ttl: int
+            The time to live
+        protocol: int
+            The protocol
+        src: str
+            The source IP address
+        dest: str
+            The destination IP address
+        data: bytes
+            The data
+    """
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, protocol, src, dest = struct.unpack("! 8x B B 2x 4s 4s", data[:20])
+    return version, header_length, ttl, protocol, get_ipv4_addr(src), get_ipv4_addr(dest), data[header_length:]
+
+def get_ipv4_addr(bytes_addr):
+    """
+    Converts the IP address to a readable format
+    
+    Args:
+        bytes_addr: bytes
+            The IP address
+            
+    Returns:
+        str
+            The IP address in format x.x.x.x
+    """
+    return ".".join(map(str, bytes_addr))
+
 if __name__ == '__main__':
     connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     print("Sniffer started")
@@ -45,3 +90,10 @@ if __name__ == '__main__':
         dest, src, protocol_type, data = unpack_ethernet_frame(raw_data)
         print("Ethernet Frame:")
         print("Destination: {}, Source: {}, Protocol: {}\n".format(dest, src, protocol_type))
+        
+        if protocol_type == 8:
+            version, header_length, ttl, protocol, src, dest, data = unpack_ipv4_packet(data)
+            print("IPv4 Packet:")
+            print("Version: {}, Header Length: {}, TTL: {}, Protocol: {}, Source: {}, Destination: {}\n".format(version, header_length, ttl, protocol, src, dest))
+            print("Data:")
+            print(textwrap.indent(data.hex(), "    "))
